aec30d4c39c70975af899bbc5d74d71744dceb34
Nathan Araujo <nathansaraujo191@gmail.com>
2024-07-27 10:11:33 -0300

    feat: adjusting some divs to use Box instead

--- src/shared/components/Infos/Infos.tsx
+++ src/shared/components/Infos/Infos.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@mui/material";
 import { ReactNode } from "react";
 
 interface InfosProps {
@@ -6,21 +7,19 @@ interface InfosProps {
 
 function Infos({ children }: InfosProps) {
   return (
-    <div style={{ width: "50%" }}>
-      <div style={{ display: "flex", height: "100%", alignItems: "center" }}>
-        <div
-          style={{
-            color: "white",
-            width: "100%",
-            maxHeight: "100%",
-            display: "grid",
-            rowGap: "1rem",
-          }}
+    <Box width={"50%"}>
+      <Box display={"flex"} height={"100%"} alignItems={"center"}>
+        <Box
+          color={"white"}
+          width={"100%"}
+          maxHeight={"100%"}
+          display={"grid"}
+          rowGap={"1rem"}
         >
           {children}
-        </div>
-      </div>
-    </div>
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
