3ccd5f5b6d7abecb71193a0a91c414832dc0e797
Nathan Araujo <nathansaraujo191@gmail.com>
2024-07-28 13:08:33 -0300

    changing vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig(() => ({
   plugins: [react()],
-  base: command === "build" ? "/psicologist-landing-page/" : "/",
+  base: "/psicologist-landing-page/",
 }));
