fc9d9ac16614e10afee646c64ee0747c820339a9
Nathan Araujo <nathansaraujo191@gmail.com>
2024-08-04 10:36:43 -0300

    feat: adjusting buttons color

--- src/shared/styles/ContactButton.ts
+++ src/shared/styles/ContactButton.ts
@@ -1,10 +1,10 @@
 import styled from "styled-components";
-import { ORANGE } from "../constants/Colors";
+import { BLUE } from "../constants/Colors";
 
 const ContactButton = styled.button`
   width: 300px;
   height: 50px;
-  background: ${ORANGE};
+  background: ${BLUE};
   border-radius: 2rem;
   border: none;
   outline: none;
@@ -12,6 +12,13 @@ const ContactButton = styled.button`
   font-weight: bold;
   color: white;
   font-size: 1em;
+
+  &:hover {
+    background: white;
+    color: ${BLUE};
+    cursor: pointer;
+    border: 3px solid ${BLUE};
+  }
 `;
 
 export default ContactButton;
