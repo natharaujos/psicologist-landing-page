46da8bea8499fde56741320e68e1ca880768cb03
nathansaraujo191 <nathansaraujo191@gmail.com>
2025-01-30 19:52:16 -0300

    feat: triying to fix a iphonee error renderiing

--- src/sections/AboutMe/AboutMe.tsx
+++ src/sections/AboutMe/AboutMe.tsx
@@ -4,7 +4,7 @@ import PictureAboutMe from "./components/PictureAboutMe/PictureAboutMe";
 function AboutMe() {
   return (
     <div className="pt-20 pb-20 flex justify-center bg-creme gap-5">
-      <section className="h-[100vh] 2xl:h-[60vh] xl:h-[105vh] lg:h-[60vh] xs:h-[1300px] w-3/4 2xl:w-4/6 xs:w-full bg-pink flex items-center rounded-md">
+      <section className="h-[100vh] 2xl:h-[60vh] xl:h-[105vh] lg:h-[60vh] xs:h-[1300px] w-3/4 2xl:w-4/6 xs:w-full bg-pink flex items-center">
         <div className="flex justify-center items-center pt-5 sm:flex-wrap xs:flex-col">
           <div className="w-1/2 flex justify-center items-start h-full sm:w-full xs:w-full">
             <PictureAboutMe />
