b683db01b694e904387860d7d301e4e089b32788
nathansaraujo191 <nathansaraujo191@gmail.com>
2025-03-09 17:38:56 -0300

    adjusting tailwind config content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,12 @@ import { PINK, CREME } from "./src/shared/constants/Colors";
 
 export default {
   darkMode: ["class"],
-  content: ["./src/**/*.{html,js,ts,tsx}"],
-  purge: {
-    content: [
-      "./src/sections/**/*.{js,jsx,ts,tsx}",
-      "./src/components/**/*.{js,jsx,ts,tsx}",
-      "./src/shared/**/*.{js,jsx,ts,tsx}",
-    ],
-  },
+  content: [
+    "./src/components/**/*.{html,js,jsx,ts,tsx}",
+    "./src/shared/**/*.{html,js,jsx,ts,tsx}",
+    "./src/sections/**/*.{html,js,jsx,ts,tsx}",
+    "./src/**/**/*.{html,js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
